b8ee928b4b2f1708e469351aceba795a648523e4
寺田泰斗 <taidonn.teeto1216@gmail.com>
2026-07-03 15:40:34 +0900

    docs: READMEとJavadocの誤字修正

--- src/main/java/com/example/tracker/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/tracker/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.tracker.exception;
 /**
  * 対象リソースが存在しない場合にスローされる例外。
  * 
- * @author trcker-team
+ * @author tracker-team
  * @since 1.0
  */
 public class ResourceNotFoundException extends RuntimeException {
